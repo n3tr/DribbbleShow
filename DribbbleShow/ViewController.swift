6fe3fc2b3469eebf81780c44cef6d54c4ec2021a
Jirat Ki <saakyz@gmail.com>
2016-08-26 19:47:51 +0700

    Initial Commit

---
+++ DribbbleShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DribbbleShow
+//
+//  Created by Jirat Ki. on 8/26/2559 BE.
+//  Copyright © 2559 n3tr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DribbbleShowTests/DribbbleShowTests.swift
@@ -0,0 +1,36 @@
+//
+//  DribbbleShowTests.swift
+//  DribbbleShowTests
+//
+//  Created by Jirat Ki. on 8/26/2559 BE.
+//  Copyright © 2559 n3tr. All rights reserved.
+//
+
+import XCTest
+@testable import DribbbleShow
+
+class DribbbleShowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
